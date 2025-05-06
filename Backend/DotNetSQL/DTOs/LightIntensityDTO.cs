bca84b3d272f9cbe238f2a12fa197b6e92fe5457
Karolina Krysiak <karolinakrysiak01@gmail.com>
2025-05-06 14:16:10 +0200

    dto for light intensity

---
+++ Backend/DotNetSQL/DTOs/LightIntensityDTO.cs
@@ -0,0 +1,8 @@
+namespace DotNetSQL.DTOs;
+
+public class LightIntensityDTO
+{
+    public int Id { get; set; }
+    public double LightIntensity { get; set; }
+    public DateTime Timestamp { get; set; }
+}
