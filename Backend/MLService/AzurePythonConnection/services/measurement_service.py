d391ea0bf54d4cc20185570d372c96d773d4d15c
Marius Gafton <mariusgafton2005@gmail.com>
2025-05-12 21:12:05 +0200

    Database connection with pyhthon to azure(without tokens, will be made later down tha line)

---
+++ Backend/MLService/AzurePythonConnection/services/measurement_service.py
@@ -0,0 +1,25 @@
+from AzurePythonConnection.services.DbConnection import get_conn
+
+def get_measurements():
+    rows = []
+    with get_conn() as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT TOP (10) [Id],
+                             [Temperature],
+                             [AirHumidity],
+                             [SoilHumidity],
+                             [Light],
+                             [Timestamp]
+            FROM [dbo].[MeasurementData]
+        """)
+        for row in cursor.fetchall():
+            rows.append({
+                "Id": row.Id,
+                "Temperature": row.Temperature,
+                "AirHumidity": row.AirHumidity,
+                "SoilHumidity": row.SoilHumidity,
+                "Light": row.Light,
+                "Timestamp": row.Timestamp
+            })
+    return rows
--- Backend/MLService/app.py
+++ Backend/MLService/app.py
@@ -1,16 +1,23 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-from flask_database_app.services.data_service import fetch_measurement_data
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from flask import Flask, jsonify # type: ignore
+from flask_cors import CORS # type: ignore
+from AzurePythonConnection.services.measurement_service import get_measurements
+from AzurePythonConnection.model.measurement_model import Measurement  
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app)
 
-@app.route('/api/measurements', methods=['GET'])
-def get_measurements():
-    data = fetch_measurement_data()
-    if not data:
-        return jsonify({"error": "No data found"}), 404
-    return jsonify(data)
+@app.route("/measurements", methods=["GET"])
+def measurements():
+    try:
+        data = get_measurements()
+        measurements = [Measurement(**item).dict() for item in data]
+        return jsonify(measurements), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
