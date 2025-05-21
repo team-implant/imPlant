3dc24628d0836ca9344b301f029e187b5920817a
JosipBr <josipbrljevic@gmail.com>
2025-05-21 13:35:18 +0200

    quick fix for allowing credentials

--- Backend/DotNetSQL/Program.cs
+++ Backend/DotNetSQL/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod();
+              .AllowCredentials();
     });
 });
 
