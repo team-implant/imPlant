5d657627d28f52727d1365c425be40bc3332ed6d
AKStifter <95703322+AKStifter@users.noreply.github.com>
2025-05-21 12:24:21 +0200

    Reverting the proto file and classes to old naming

--- IoT/IoT_server/TcpGrpcBridgeServer/Models/SoilMeasurement.cs
+++ IoT/IoT_server/TcpGrpcBridgeServer/Models/SoilMeasurement.cs
@@ -3,7 +3,7 @@ namespace TcpGrpcBridgeServer.Models
     public class SoilMeasurement
     {
         public int PlantId { get; set; }
-        public DateTime MeasureId { get; set; }
+        public int MeasureId { get; set; }
         public float Value { get; set; }
     }
 }
