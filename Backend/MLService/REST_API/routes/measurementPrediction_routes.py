bd57f69893c51d12616b59e60cb6c27290d591f6
Karolina Krysiak <karolinakrysiak01@gmail.com>
2025-05-22 10:55:05 +0200

    Update measurementPrediction_routes.py

--- Backend/MLService/REST_API/routes/measurementPrediction_routes.py
+++ Backend/MLService/REST_API/routes/measurementPrediction_routes.py
@@ -1,14 +1,21 @@
 from flask import Blueprint, jsonify # type: ignore
-#from ML.models import measurement_linearRegression
+from ML.models.Measurement_models import measurement_randomForest
+from REST_API.services.general_prediction_service import get_predictions
 
 measurement_predictions_bp = Blueprint("measurement_predictions", __name__)
 
 @measurement_predictions_bp.route("/predictions/measurements", methods=["GET"])
 def get_measurements_route():
     try:
-       # data = measurement_linearRegression.get_measurement_predictions_json()
-       # return jsonify(data), 200
-        return "implement measurement model", 200
+        data = measurement_randomForest.get_measurement_predictions_json()
+        return jsonify(data), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
+
+@measurement_predictions_bp.route("/predictions/measurements/forecast", methods=["GET"])
+def forecast_multi_measurements():
+    try:
+        forecast = get_predictions("Measurement")
+        return jsonify(forecast), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
