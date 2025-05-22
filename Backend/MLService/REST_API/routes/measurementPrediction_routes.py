cdbb90c7558b303073f2ac7bff629cd566c6a60c
Karolina Krysiak <karolinakrysiak01@gmail.com>
2025-05-22 11:06:52 +0200

    added gradient boosting route

--- Backend/MLService/REST_API/routes/measurementPrediction_routes.py
+++ Backend/MLService/REST_API/routes/measurementPrediction_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify # type: ignore
 from ML.models.Measurement_models import measurement_randomForest
 from REST_API.services.general_prediction_service import get_predictions
+from ML.models.Measurement_models import measurement_randomForest, measurement_gradientBoosting
 
 measurement_predictions_bp = Blueprint("measurement_predictions", __name__)
 
@@ -11,6 +12,16 @@ def get_measurements_route():
         return jsonify(data), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+# it uses all measurment valueas to make predictions, but may make more sense to put it with soil humidity 
+@measurement_predictions_bp.route("/predictions/measurements/gradient_boosting", methods=["GET"]) 
+def get_measurements_gradient_boosting_route():
+    try:
+        data = measurement_gradientBoosting.get_soil_humidity_predictions_json()
+        return jsonify(data), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 @measurement_predictions_bp.route("/predictions/measurements/forecast", methods=["GET"])
 def forecast_multi_measurements():
