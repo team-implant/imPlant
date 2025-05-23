b91000a0115075dba120222251e04fb4f7ad8528
Samo Susa <samosusa123@gmail.com>
2025-05-23 10:11:52 +0200

    removed soilmesurement table entity

--- Backend/DotNetSQL/DTOs/SoilMeasurementDto.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNetSQL.DTOs
-{
-    public class SoilMeasurementDto
-    {
-        public int PlantId { get; set; }
-        public int MeasurementId { get; set; }
-        public double Value { get; set; }
-    }
-}
