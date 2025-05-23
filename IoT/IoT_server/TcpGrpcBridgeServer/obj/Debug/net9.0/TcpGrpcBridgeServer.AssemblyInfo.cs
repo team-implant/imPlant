7761317b05794602a1db5868da2c540648e0e5b0
bibek344326-viaSTE <344326@via.dk>
2025-05-23 10:26:33 +0200

    minor fixes to data parser

--- IoT/IoT_server/TcpGrpcBridgeServer/obj/Debug/net9.0/TcpGrpcBridgeServer.AssemblyInfo.cs
+++ IoT/IoT_server/TcpGrpcBridgeServer/obj/Debug/net9.0/TcpGrpcBridgeServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TcpGrpcBridgeServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e3c4d4360f890a35fe1211674403788af4c330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4148096ea1afeb141ca42fc221708554dd3964d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TcpGrpcBridgeServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TcpGrpcBridgeServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
