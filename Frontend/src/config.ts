1d8f54c156e424396a6d5adad0e52771260aec33
Josip Brljevic <josipbrljevic@gmail.com>
2025-05-26 12:11:48 +0200

    changing to olegs server

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://sep4-implant.azurewebsites.net/api";
+export const BASE_URL = "https://implantapi.electimore.xyz/api/";
 export const ML_BASE_URL = "http://159.89.1.205:8080";
