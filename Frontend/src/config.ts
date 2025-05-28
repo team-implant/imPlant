8f6983ab3d31d4069530309c055c6edbbc4d06a8
Josip Brljevic <josipbrljevic@gmail.com>
2025-05-28 10:59:07 +0200

    fixing the endpoints

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = "https://implantapi.electimore.xyz/api/";
-export const ML_BASE_URL = "https://159.89.1.205/";
+export const ML_BASE_URL = "https://implantml.electimore.xyz/";
